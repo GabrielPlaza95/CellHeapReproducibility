32d24e58ad04db63797f865e0dfc380fa4d299b5
Gabriel Plaza <gabrielplaza@hotmail.com.br>
2024-07-01 19:54:16 -0300

    Ajuste medição de tempo. Contém alterações inacabadas da fase 4

--- Source/Phase4/cluster.R
+++ Source/Phase4/cluster.R
@@ -1,3 +1,4 @@
+library(cellassign)
 library(DropletUtils)
 library(Seurat)
 library(dplyr)
@@ -11,14 +12,15 @@ library(MAST)
 library(SingleCellExperiment)
 library(Matrix)
 library(reshape2)
-library(cellassign)
 library(org.Hs.eg.db)
 library(edgeR)
 library(scater)
 library(loomR)
+library(reticulate)
 
 # On error, save debugging info to file last.dump.rda
 dump_and_quit <- function() {
+  print(reticulate::py_last_error())
   dump.frames(to.file = TRUE)
   quit(status = 1)
 }
@@ -31,7 +33,6 @@ if (length(args)==0) {
 }
 
 PROJECT_DIR <- Sys.getenv("PROJECT_DIR")
-
 
 IN_DIR <- file.path(PROJECT_DIR, "out", "phase3")
 OUT_DIR <- file.path(PROJECT_DIR, "out", "phase4")
@@ -71,15 +72,11 @@ features <- SelectIntegrationFeatures(object.list = alldata.list)
 
 immune.anchors <- FindIntegrationAnchors(object.list = alldata.list, anchor.features = features)
 
-print(immune.anchors)
-
 ##########
 
 #Create the integrated assays
 immune.combined <- IntegrateData(anchorset = immune.anchors)
 DefaultAssay(immune.combined) <- "integrated"
-
-print(immune.combined)
 
 # ScaleData() pattern - to dimension high variable genes (previously identified - 2000). It does not affect the PCA and clustering results.
 immune.combined <- ScaleData(immune.combined)
@@ -93,8 +90,15 @@ immune.combined <- JackStraw(immune.combined, num.replicate = 100)
 immune.combined <- ScoreJackStraw(immune.combined, dims = 1:20)
 
 # Graphs related to dimension test
+dpi=300
+png(file=file.path(OUT_DIR, "dimension_test_jackstraw.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
 JackStrawPlot(immune.combined, dims = 1:20)
+dev.off()
+
+dpi=300
+png(file=file.path(OUT_DIR, "dimension_test_elbow.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
 ElbowPlot(immune.combined)
+dev.off()
 
 # UMAP - visualization and exploration of datasets. UMAP input - Seurat suggestion, same PCs as input to the clustering analysis
 # UMAP clustering
@@ -115,8 +119,6 @@ DefaultAssay(immune.combined) <- "RNA"
 
 # To convert Seurat object into SingleCellExperiment object
 immune.combined = JoinLayers(immune.combined)
-
-print(immune.combined)
 
 test.sce <- as.SingleCellExperiment(immune.combined)
 
@@ -162,9 +164,6 @@ marker_gene_list <- list(
 #TODO voltar a usar a lista real
 #marcadores <- marker_list_to_mat(marker_gene_list, include_other = FALSE)
 
-print(test.sce)
-#print(rownames(test.sce))
-
 marker_gene_list_teste <- list(
   TCD4Th1 = sample(rownames(test.sce), 3),
   TCD4Th2 = sample(rownames(test.sce), 6)
@@ -178,20 +177,17 @@ stopifnot(all(!is.na(marcadores_teste)))
 test.sce <- test.sce[marcadores_teste,]
 stopifnot(all.equal(rownames(marcadores), rownames(test.sce)))
 
-#TODO resolve erro do cellassign mas por que este filtro é necessário?
+# Garante que não nenhuma coluna ou linha esteja vazia
 test.sce <- test.sce[which(rowSums(counts(test.sce)) > 0),]
 test.sce <- test.sce[,which(colSums(counts(test.sce)) > 0)]
 
-#Ajusta lista de marcadores após filtro anterior
+# Ajusta lista de marcadores após filtro anterior
 shared <- intersect(rownames(marcadores), rownames(test.sce))
 marcadores <- marcadores[shared,]
 
 # Size Factors
 test.sce <- scran::computeSumFactors(test.sce)
 s1 <- sizeFactors(test.sce, onAbsence = "warn")
-
-print(marcadores)
-print(test.sce)
 
 # Cellassign fit - based on cellassign vignette
 # https://irrationone.github.io/cellassign/articles/introduction-to-cellassign.html#constructing-a-marker-gene-matrix
@@ -210,70 +206,70 @@ test.sce$cellassign_type <- celltypes(fit,assign_prob = 0.95)
 table(test.sce$cellassign_type)
 
 # Output file names - total_violin_XX.png - it is possible to create other output files formats
-dpi=300
-png(file=file.path(OUT_DIR, "total_violin_fc.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotExpression(test.sce, features = c("human----FCGRT","human----FCGR1A", "human----FGL2", "human----FCGR2B", "human----FCAR","human----FCER1A","human----TRIM21", "human----FCGR3A", "human----FCGR3B","human----FCGR2A"), x = "cellassign_type",colour_by = 'cellassign_type')
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_violin_fc2.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotExpression(test.sce, features = c("human----FCGRT","human----FCGR1A", "human----FGL2", "human----FCGR2B"), x = "cellassign_type",colour_by = 'cellassign_type')
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_violin_fc3.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotExpression(test.sce, features = c("human----FCAR","human----FCER1A","human----TRIM21", "human----FCGR3A", "human----FCGR3B"), x = "cellassign_type",colour_by = 'cellassign_type')
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_violin_vs.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotExpression(test.sce, features = c("human----B2M","human----FCER1G", "human----HCK", "human----SYK","human----RAF1", "human----MAPK8","human----MAPK11","human----MAPK1", "human----LYN", "human----FYN", "human----LAT","human----LCP2", "human----PTPN6", "human----PTPN11","human----INPP5D", "human----BTK","human----UBE2W","human----UBE2N", "human----UBE2V2", "human----PSMD14", "human----IKBKG","human----NFKB1", "human----NFKB2", "human----VCP","human----CGAS", "human----DDX58"), x = "cellassign_type",colour_by = 'cellassign_type')
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_violin_vs2.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotExpression(test.sce, features = c("human----B2M","human----FCER1G", "human----HCK", "human----SYK","human----RAF1", "human----MAPK8","human----MAPK11","human----MAPK1"), x = "cellassign_type",colour_by = 'cellassign_type')
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_violin_vs3.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotExpression(test.sce, features = c("human----LYN", "human----FYN", "human----LAT","human----LCP2", "human----PTPN6", "human----PTPN11","human----INPP5D", "human----BTK"), x = "cellassign_type",colour_by = 'cellassign_type')
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_violin_vs4.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotExpression(test.sce, features = c("human----UBE2W","human----UBE2N", "human----UBE2V2", "human----PSMD14", "human----IKBKG","human----NFKB1", "human----NFKB2", "human----VCP","human----CGAS", "human----DDX58"), x = "cellassign_type",colour_by = 'cellassign_type')
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_violin_cito.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotExpression(test.sce, features = c("human----IFNG", "human----IFNA1", "human----TNF", "human----IL18","human----IL1B", "human----IL10","human----CCL5","human----IL6", "human----IL4", "human----CCL2", "human----CXCL10","human----CCL3", "human----CXCL8","human----CX3CL1", "human----TGFB1"), x = "cellassign_type",colour_by = 'cellassign_type')
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_violin_cito2.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotExpression(test.sce, features = c("human----IFNG","human----IFNA1", "human----TNF", "human----IL18","human----IL1B", "human----IL10","human----CCL5","human----IL6"), x = "cellassign_type",colour_by = 'cellassign_type')
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_violin_cito3.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotExpression(test.sce, features = c("human----IL4", "human----CCL2", "human----CXCL10","human----CCL3", "human----CXCL8","human----CX3CL1", "human----TGFB1"), x = "cellassign_type",colour_by = 'cellassign_type')
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_dots_fc.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotDots(test.sce, features = c("human----FCGRT","human----FCGR1A", "human----FGL2", "human----FCGR2B","human----FCAR","human----FCER1A","human----TRIM21", "human----FCGR3A", "human----FCGR3B"), group='cellassign_type')
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_dots_vs.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotDots(test.sce, features = c("human----B2M","human----FCER1G", "human----HCK", "human----SYK","human----RAF1", "human----MAPK8","human----MAPK11","human----MAPK1", "human----LYN", "human----FYN", "human----LAT","human----LCP2", "human----PTPN6", "human----PTPN11","human----INPP5D", "human----BTK","human----UBE2W","human----UBE2N", "human----UBE2V2", "human----PSMD14", "human----IKBKG","human----NFKB1", "human----NFKB2", "human----VCP","human----CGAS", "human----DDX58"), group='cellassign_type')
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_dots_cito2.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotDots(test.sce, features = c("human----IFNG", "human----TNF", "human----IL18","human----IL1B", "human----IL10","human----CCL5","human----IL6", "human----IL4", "human----CCL2", "human----CXCL10","human----CCL3", "human----CXCL8","human----CX3CL1", "human----TGFB1"), group='cellassign_type')
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_heatmap_fc.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotHeatmap(test.sce, features = c("human----FCGRT","human----FCGR1A", "human----FGL2", "human----FCGR2B","human----FCAR","human----FCER1A","human----TRIM21", "human----FCGR3A", "human----FCGR3B"))
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_heatmap_vs.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotHeatmap(test.sce, features = c("human----B2M","human----FCER1G", "human----HCK", "human----SYK","human----RAF1", "human----MAPK8","human----MAPK11","human----MAPK1", "human----LYN", "human----FYN", "human----LAT","human----LCP2", "human----PTPN6", "human----PTPN11","human----INPP5D", "human----BTK","human----UBE2W","human----UBE2N", "human----UBE2V2", "human----PSMD14", "human----IKBKG","human----NFKB1", "human----NFKB2", "human----VCP","human----CGAS", "human----DDX58"))
-dev.off()
-
-png(file=file.path(OUT_DIR, "total_heatmap_cito.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
-plotHeatmap(test.sce, features = c("human----IFNG","human----IFNA1", "human----TNF", "human----IL18","human----IL1B", "human----IL10","human----CCL5","human----IL6", "human----IL4", "human----CCL2", "human----CXCL10","human----CCL3", "human----CXCL8","human----CX3CL1", "human----TGFB1"))
-dev.off()
+# dpi=300
+# png(file=file.path(OUT_DIR, "total_violin_fc.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotExpression(test.sce, features = c("human----FCGRT","human----FCGR1A", "human----FGL2", "human----FCGR2B", "human----FCAR","human----FCER1A","human----TRIM21", "human----FCGR3A", "human----FCGR3B","human----FCGR2A"), x = "cellassign_type",colour_by = 'cellassign_type')
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_violin_fc2.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotExpression(test.sce, features = c("human----FCGRT","human----FCGR1A", "human----FGL2", "human----FCGR2B"), x = "cellassign_type",colour_by = 'cellassign_type')
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_violin_fc3.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotExpression(test.sce, features = c("human----FCAR","human----FCER1A","human----TRIM21", "human----FCGR3A", "human----FCGR3B"), x = "cellassign_type",colour_by = 'cellassign_type')
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_violin_vs.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotExpression(test.sce, features = c("human----B2M","human----FCER1G", "human----HCK", "human----SYK","human----RAF1", "human----MAPK8","human----MAPK11","human----MAPK1", "human----LYN", "human----FYN", "human----LAT","human----LCP2", "human----PTPN6", "human----PTPN11","human----INPP5D", "human----BTK","human----UBE2W","human----UBE2N", "human----UBE2V2", "human----PSMD14", "human----IKBKG","human----NFKB1", "human----NFKB2", "human----VCP","human----CGAS", "human----DDX58"), x = "cellassign_type",colour_by = 'cellassign_type')
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_violin_vs2.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotExpression(test.sce, features = c("human----B2M","human----FCER1G", "human----HCK", "human----SYK","human----RAF1", "human----MAPK8","human----MAPK11","human----MAPK1"), x = "cellassign_type",colour_by = 'cellassign_type')
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_violin_vs3.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotExpression(test.sce, features = c("human----LYN", "human----FYN", "human----LAT","human----LCP2", "human----PTPN6", "human----PTPN11","human----INPP5D", "human----BTK"), x = "cellassign_type",colour_by = 'cellassign_type')
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_violin_vs4.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotExpression(test.sce, features = c("human----UBE2W","human----UBE2N", "human----UBE2V2", "human----PSMD14", "human----IKBKG","human----NFKB1", "human----NFKB2", "human----VCP","human----CGAS", "human----DDX58"), x = "cellassign_type",colour_by = 'cellassign_type')
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_violin_cito.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotExpression(test.sce, features = c("human----IFNG", "human----IFNA1", "human----TNF", "human----IL18","human----IL1B", "human----IL10","human----CCL5","human----IL6", "human----IL4", "human----CCL2", "human----CXCL10","human----CCL3", "human----CXCL8","human----CX3CL1", "human----TGFB1"), x = "cellassign_type",colour_by = 'cellassign_type')
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_violin_cito2.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotExpression(test.sce, features = c("human----IFNG","human----IFNA1", "human----TNF", "human----IL18","human----IL1B", "human----IL10","human----CCL5","human----IL6"), x = "cellassign_type",colour_by = 'cellassign_type')
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_violin_cito3.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotExpression(test.sce, features = c("human----IL4", "human----CCL2", "human----CXCL10","human----CCL3", "human----CXCL8","human----CX3CL1", "human----TGFB1"), x = "cellassign_type",colour_by = 'cellassign_type')
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_dots_fc.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotDots(test.sce, features = c("human----FCGRT","human----FCGR1A", "human----FGL2", "human----FCGR2B","human----FCAR","human----FCER1A","human----TRIM21", "human----FCGR3A", "human----FCGR3B"), group='cellassign_type')
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_dots_vs.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotDots(test.sce, features = c("human----B2M","human----FCER1G", "human----HCK", "human----SYK","human----RAF1", "human----MAPK8","human----MAPK11","human----MAPK1", "human----LYN", "human----FYN", "human----LAT","human----LCP2", "human----PTPN6", "human----PTPN11","human----INPP5D", "human----BTK","human----UBE2W","human----UBE2N", "human----UBE2V2", "human----PSMD14", "human----IKBKG","human----NFKB1", "human----NFKB2", "human----VCP","human----CGAS", "human----DDX58"), group='cellassign_type')
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_dots_cito2.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotDots(test.sce, features = c("human----IFNG", "human----TNF", "human----IL18","human----IL1B", "human----IL10","human----CCL5","human----IL6", "human----IL4", "human----CCL2", "human----CXCL10","human----CCL3", "human----CXCL8","human----CX3CL1", "human----TGFB1"), group='cellassign_type')
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_heatmap_fc.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotHeatmap(test.sce, features = c("human----FCGRT","human----FCGR1A", "human----FGL2", "human----FCGR2B","human----FCAR","human----FCER1A","human----TRIM21", "human----FCGR3A", "human----FCGR3B"))
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_heatmap_vs.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotHeatmap(test.sce, features = c("human----B2M","human----FCER1G", "human----HCK", "human----SYK","human----RAF1", "human----MAPK8","human----MAPK11","human----MAPK1", "human----LYN", "human----FYN", "human----LAT","human----LCP2", "human----PTPN6", "human----PTPN11","human----INPP5D", "human----BTK","human----UBE2W","human----UBE2N", "human----UBE2V2", "human----PSMD14", "human----IKBKG","human----NFKB1", "human----NFKB2", "human----VCP","human----CGAS", "human----DDX58"))
+# dev.off()
+# 
+# png(file=file.path(OUT_DIR, "total_heatmap_cito.png"), width = dpi*20, height = dpi*14, units = "px",res = dpi,type='cairo')
+# plotHeatmap(test.sce, features = c("human----IFNG","human----IFNA1", "human----TNF", "human----IL18","human----IL1B", "human----IL10","human----CCL5","human----IL6", "human----IL4", "human----CCL2", "human----CXCL10","human----CCL3", "human----CXCL8","human----CX3CL1", "human----TGFB1"))
+# dev.off()
 
 #Counting number of cellsTCD4Th1
 TCD4Th1.only <- test.sce[, test.sce$cellassign_type == "TCD4Th1"]
